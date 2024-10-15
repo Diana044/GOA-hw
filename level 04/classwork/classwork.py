43ae157d5bc60d44f930c7d483392a48864d6a11
Diana-svg <dzukaevadiana3@gmail.com>
2024-10-15 20:27:01 +0400

    commit

---
+++ level 04/classwork/classwork.py
@@ -0,0 +1,21 @@
+
+#string
+name="Diana"
+surname="Dzuaevi"
+
+num1=278
+num2=20.5
+
+print(name + surname)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num2 * num1)
+print(num2 / num1)
+
+
+# concatenate- კონკატინაცია
+num0="10,65"
+num=378373873821
+
+print(name + num0)
