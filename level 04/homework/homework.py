4650c5a4577d91b96db967939ad1bee16be67c5d
Diana-svg <dzukaevadiana3@gmail.com>
2024-10-16 18:43:50 +0400

    commit

---
+++ level 04/homework/homework.py
@@ -0,0 +1,29 @@
+ #1
+num1=323322
+num2=434344
+num3=4
+
+print(num1 + num2 + num3)
+
+#2
+name="Diana"
+
+#3 float
+height=163
+ 
+#4 
+x = 15
+x = x + 5
+
+print(x)
+
+#5
+my_name="Diana"
+my_surname="Dzukaevi"
+
+print(my_name + " "+  my_surname)
+
+#6
+price=100
+x=5
+print(x * price)
