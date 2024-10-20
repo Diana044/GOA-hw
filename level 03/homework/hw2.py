29fb5d868ce1e92b73c799dc386bbdad9a54560f
Diana-svg <dzukaevadiana3@gmail.com>
2024-10-20 22:40:43 +0400

    commit

---
+++ level 03/homework/hw2.py
@@ -0,0 +1,34 @@
+
+# 5) Turtle ბიბლიოთეკის დახმარებით დახატეთ ფასანაურული ხინკალი
+
+
+from turtle import *
+
+#xinkhali
+speed(40)
+
+begin_fill()
+width(4)
+color("wheat")
+forward(340)
+left(90)
+forward(150)
+left(90)
+forward(130)
+right(90)
+forward(70)
+left(90)
+forward(80)
+left(90)
+forward(70)
+right(90)
+forward(130)
+left(90) 
+forward(100)
+end_fill()
+
+
+
+
+exitonclick()
+
