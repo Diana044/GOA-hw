2230544ac5fc010f2ed1bf0298c7a3558bf32503
Diana-svg <dzukaevadiana3@gmail.com>
2024-11-12 22:05:23 +0400

    commit

---
+++ level 12/classwork/cw.py
@@ -0,0 +1,20 @@
+# comparison operations-
+# data types
+# ints, str, float, boolean
+
+# print(6<3)
+
+# true
+# false
+
+# =მიმნიჭებელი ოპერატორი
+#  == შედარების ოპერატორი
+
+# print(type(True))
+
+print(5 == 7)
+
+# user_age = int(input("write ur age") )
+# if user_age >18 == False
+
+# print(user_age)
---
+++ level 12/classwork/cw2.py
@@ -0,0 +1,12 @@
+# level 12:
+#    1) შეადარეთ ორი რიცხვი ერთმანეთს ყველა შესაძლო შედარების ოპერატორის გამოყენებით, ახსენით კომენტარის სახით თქვენი სიტყვებით რა არის boolean ი და რაში გვჭირდება შედარების ოპერატორები
+
+print(54 > 4)
+print(4 < 66)
+print(555 == 44)
+print(34 <= 5)
+print( 66 >= 66)
+
+# boolean - aris monacemta tipi romelsac aqvs ori shesadzlo mishvneloba, true or false.
+# shedareis opratorebi gvchirdeba imistvis rom davadginot cvladis chesmarteba an sicrue
+
