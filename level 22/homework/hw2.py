569fc9761b80496942890f8fe0f3d6f0ba741294
Diana044 <dzukaevadiana3@gmail.com>
2024-12-23 20:03:52 +0400

    commit

--- level 22/homework/hw2.py
+++ level 22/homework/hw2.py
@@ -1,6 +1,6 @@
 #     2) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი ერთიდან ამ რიცხვამდე დაბეჭდეთ რიცხვების საშუალო არითმეტიკული
-# average=0
-# num=int(input('enter number'))
-# for i in range(num):
-#     average=average+num/
-# print(average)
+average=0
+num=int(input('enter number'))
+for i in range(num):
+    average=average+num
+print(average)
