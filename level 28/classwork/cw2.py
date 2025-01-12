c27ecff9bbe33e5ee348f109f49edc9b6734c7c0
Diana044 <dzukaevadiana3@gmail.com>
2025-01-13 01:31:40 +0400

    Create cw2.py

---
+++ level 28/classwork/cw2.py
@@ -0,0 +1,17 @@
+# 2)შექმენით სია, რომელიც შეიცავს 5 სხვადასხვა ხილის სახელს. შეცვალეთ მესამე ხილის სახელი და გამოიტანეთ განახლებული სია.
+
+fruit_list=['apple','orange','banana','kiwi','pinapple',]
+print(fruit_list)
+
+fruit_list[2]='cherry'
+print(fruit_list)
+
+
+# numbers=[1,2,3,4,5]
+# print(numbers)
+
+# numbers.append(6)
+# print(numbers)
+# print(len(numbers))
+# numbers.pop(1)
+# print(numbers)
