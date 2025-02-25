101e9c5cbb04882d00c9b197b04049806c9bca95
Diana044 <dzukaevadiana3@gmail.com>
2025-02-25 14:56:12 +0400

    commit

---
+++ level 41/homework/hw1.py
@@ -0,0 +1,6 @@
+def summation(num):
+    summ=0
+    for i in range(num+1):
+        summ+=i
+            
+    return summ
---
+++ level 41/homework/hw2.py
@@ -0,0 +1,7 @@
+def count_sheeps(sheep):
+    for_True=[]
+    for i in sheep:
+        if i==True:
+            for_True.append(i)
+    return len(for_True)
+            
