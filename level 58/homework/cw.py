e17df902504fb1e22d2f9294ad521f97292b2af4
Diana044 <dzukaevadiana3@gmail.com>
2025-04-28 19:11:12 +0400

    commit

--- level 58/homework/cw.py
+++ level 58/homework/cw.py
@@ -19,3 +19,9 @@ def capitalize(s):
             
 
     return [even, odd]
+
+
+# 2) ახსენით რა არის transition და რისთვის ვიყენებთ. განმარტეთ თითოეული ქვე-კუთვნილება: transition-duration, transition-property, transition-timing-function
+#transition propertyt vakonkretebt tu romeli css propertystvis aris gawerili efeqti
+# transition-durationit vwert tu ramdeni wamis an miliwamis ganmavlobashui unda xdebodes qmedeba tu efeqti elementze ,
+#  transition-timing-function- efeqtis sichqare
