84bbac873b3a09f1785fbc232d147b8d7c183ce4
Diana044 <dzukaevadiana3@gmail.com>
2025-09-18 22:13:18 +0400

    commit

---
+++ level 101/classwork/cw.js
@@ -0,0 +1,19 @@
+// // 1)while loop ის დახმარებით დაბეჭდეთ რიცხვ ები 1 დან 100მდე
+
+// 2)while loop ის დახმარებით დაბეჭდეთ 192000757
+
+// 1 I Love You
+// 2 I Love You
+// 3 I Love You 
+// ... 
+// 192000757 I Love You
+let i=1
+while (i <=100){
+    console.log(i)
+    i++
+}
+
+while (i<192000757){
+    console.log(i,'i love  you')
+    i++
+}
