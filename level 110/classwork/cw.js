eab889554c0b8dc8fcd7c5b6d816e92ac7edb027
Diana044 <dzukaevadiana3@gmail.com>
2025-10-21 17:34:46 +0400

    commit

--- level 110/classwork/cw.js
+++ level 110/classwork/cw.js
@@ -1,22 +1,22 @@
 // level 110:
 //     1) შექმენით სია რომელშიც იქნება რიცხვები, შემდეგ შექმენით ორი დამატებითი სია, ერთ სიაში ჩაამატეთ მთავარი სიიდან მხოლოდ ლუწი რიცხვები ხოლო მეორეში მხოლოდ კენტები
 
-// let numsArr = [3, 432, 543, 55, 6646, 3, 2, 4]
+let numsArr = [3, 432, 543, 55, 6646, 3, 2, 4]
 
-// let evenArr = []
-// let oddArr = []
-// let i = 0
+let evenArr = []
+let oddArr = []
+let i = 0
 
-// while (i < numsArr.length) {
-//     if (numsArr[i] % 2 == 0) {
-//         evenArr.push(numsArr[i])
-//     } else {
-//         oddArr.push(numsArr[i])
-//     }
-//     i++
-// }
+while (i < numsArr.length) {
+    if (numsArr[i] % 2 == 0) {
+        evenArr.push(numsArr[i])
+    } else {
+        oddArr.push(numsArr[i])
+    }
+    i++
+}
 
-// console.log( evenArr)
-// console.log(oddArr)
+console.log( evenArr)
+console.log(oddArr)
 
 
---
+++ level 110/homework/hw.js
@@ -0,0 +1,27 @@
+
+function validParentheses(parenStr) {
+  if (parenStr.length === 0) return true
+  let count1=0
+  let count2=0
+  
+  for(let i=0;i<parenStr.length; i++){
+    if(parenStr[i]=='('){
+      count1++
+    }else if (parenStr[i]== ")"){
+      count2++
+    }
+    if (count2 > count1){
+      return false
+  }
+  }
+  
+  
+  if(count1===count2 && (parenStr[0]==='(' && parenStr[parenStr.length-1]===')')){
+    return true
+  }else{
+    return false
+  }
+}
+
+console.log(validParentheses('((())()((()()('))
+console.log(validParentheses('()()'))
