f6bb24205ef9283f875832c09722939c0cb59f26
Diana <dzukaevadiana3@gmail.com>
2025-10-30 16:53:06 +0400

    commit

--- level 112/homework/hw.js
+++ level 112/homework/hw.js
@@ -9,36 +9,3 @@ function letterCheck(arr) {
   }
   return true  
 }
-function top3(products, amounts, prices) {
-  let revenues = []
-  for (let i = 0; i < products.length; i++) {
-    revenues.push(amounts[i] * prices[i])
-  }
-
-  let max = 0
-  for (let i = 0; i < revenues.length; i++) {
-    if (revenues[i] > revenues[max]) {
-      max = i
-    }
-  }
-
-  let middle = -1
-  for (let i = 0; i < revenues.length; i++) {
-    if (i !== max) {
-      if (middle === -1 || revenues[i] > revenues[middle]) {
-        middle = i
-      }
-    }
-  }
-
-  let min = -1
-  for (let i = 0; i < revenues.length; i++) {
-    if (i !== max && i !== middle) {
-      if (min === -1 || revenues[i] > revenues[min]) {
-        min = i
-      }
-    }
-  }
-
-  return [products[max], products[middle], products[min]]
-}
