2dd15a08bc919edb632374f4bc24f2f2a90cfbd4
Diana <dzukaevadiana3@gmail.com>
2025-10-31 18:56:00 +0400

    commit

--- level 112/homework/hw.js
+++ level 112/homework/hw.js
@@ -9,3 +9,55 @@ function letterCheck(arr) {
   }
   return true  
 }
+
+
+function factorial(n){
+  if (n < 0 || n > 12) {
+    throw new RangeError();
+  }
+  if (n===0){
+    return 1
+  }
+  
+  let result=1
+  for(let i=1;i<=n; i++){
+    result=result *i
+  }
+  return result
+}
+// #2
+function factorial(n){
+  if (n < 0 || n > 12) {
+    throw new RangeError();
+  }
+  if (n===0){
+    return 1
+  }
+  
+  let result=1
+  for(let i=n;i>=1; i--){
+    result=result *i
+  }
+  return result
+}
+
+function tiyFizzBuzz(sentence) {
+  let newStr = ''
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i]
+
+    if ((char >= 'A' && char <= 'Z') && 
+      !(char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U')) {
+      newStr += 'Iron'
+      }else if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+      newStr += 'Iron Yard'
+      }else if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      newStr += 'Yard'
+    } else {
+      newStr += char
+    }
+  }
+
+  return newStr
+}
