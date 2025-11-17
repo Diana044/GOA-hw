02b86aa80f969fd8369b7bfa7969fb729a412732
Diana <dzukaevadiana3@gmail.com>
2025-11-17 21:28:26 +0400

    commit

--- level 116/homework/js/hw8.js
+++ level 116/homework/js/hw8.js
@@ -1,11 +1,13 @@
 // 8)შექმენი 5 ცალი i თეგი და მოათავსე რაიმე ტექსტი შიგნით,შენი დავცალებაა რომ წამოიღო ყველა i თეგი და შეინახო ცვლადში,შენი დავალებააა რომ ყველა i თეგში მყოფი ტექსტი textContent ის გამოყენებით შეცვალო --> "viri" ით /გამოიყენე while loop/for loop ორივე
 
 const italic=document.getElementsByTagName('i')
-let i=0
+
+for(let i of italic){
+    i.textContent='viri'
+}
+let i =0
+
 while(i<italic.length){
     italic[i].textContent='viri'
-}
-
-for(i of italic){
-    i.textContent='viri'
+    i++
 }
