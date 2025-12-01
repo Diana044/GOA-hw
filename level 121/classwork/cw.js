6e65e2e531125c8ada2221d390e08c681e378d19
Diana <dzukaevadiana3@gmail.com>
2025-12-01 23:00:53 +0400

    commit

---
+++ level 121/classwork/cw.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form')
+const div=document.getElementById('div')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let name=e.target.name.value
+    let surnaame=e.target.surname.value
+    let age=e.target.age.value
+
+
+})
+
