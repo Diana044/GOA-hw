58db82f6941f2c03ac130813406235164365f041
Diana <dzukaevadiana3@gmail.com>
2026-01-11 01:04:08 +0400

    commit

--- level 129/homework/interactive-card-details-form-main/interactive-card-details-form-main/script.js
+++ level 129/homework/interactive-card-details-form-main/interactive-card-details-form-main/script.js
@@ -8,7 +8,8 @@ let form=document.getElementById('form')
 
 let cardNameSpan=document.getElementById('cardNameSpan')
 let cardNumberSpan=document.getElementById('cardNumberSpan')
-
+let expDateSpan=document.getElementById('expDateSpan')
+let cvcSpan=document.getElementById('cvcSpan')
 
 
 form.addEventListener('submit',function(e){
@@ -20,29 +21,65 @@ form.addEventListener('submit',function(e){
     let expDateYear=e.target.expDateYear.value
     let CVC=e.target.CVC.value
 
-    cardName=false
+    let cardNamCorrect=false
     if(e.target.cardName.value===''){
         cardNameSpan.style.display='flex'
+        e.target.cardName.style.borderColor='red'
     }else{
         cardNameSpan.style.display='none'
-        cardName=true
+        e.target.cardName.style.borderColor='hsl(270, 3%, 87%)'
+        cardNamCorrect=true
     }
     
-    cardNumber=false
+    let cardNumbeCorrect=false
     if(e.target.cardNumber.value===''){
         cardNumberSpan.innerHTML="Can't be blank"
         cardNumberSpan.style.display='flex'
-    }else{
+        e.target.cardNumber.style.borderColor='red'
+    }
+    else{
+        for(let i of e.target.cardNumber.value){
+            if(!('1234567890'.includes(i))){
+                cardNumberSpan.innerHTML='Wrong format, number only'
+                cardNumberSpan.style.display='flex'
+                return
+            }
+        }
         cardNumberSpan.style.display='none'
-        cardNumber=true
+        e.target.cardNumber.style.borderColor='hsl(270, 3%, 87%)'
+        cardNumbeCorrect=true
     }
 
-      cardNumber=false
-    if(e.target.cardNumber.value.type!=Number){
-        cardNumberSpan.style.display='flex'
-        cardNumberSpan.innerHTML="Wrong format, numbers only"
-    }else{
-        cardNumberSpan.style.display='none'
-        cardNumber=true
+    let expMonthCorrect=false
+    if(expDateMonth==='' || expDateYear  ===''){
+        expDateSpan.style.display='flex'
+        if(expDateMonth==='' && expDateYear!==''){
+            e.target.expDateMonth.style.borderColor='red'
+            e.target.expDateYear.style.borderColor=' hsl(270, 3%, 87%)'
+        }else if(expDateYear  ==='' && expDateMonth!==''){
+            e.target.expDateYear.style.borderColor='red'
+            e.target.expDateMonth.style.borderColor=' hsl(270, 3%, 87%)'
+        }
+        else{
+            e.target.expDateMonth.style.borderColor='red'
+            e.target.expDateYear.style.borderColor='red'
+        }
     }
+    else{
+        expDateSpan.style.display='none'
+        e.target.expDateMonth.style.borderColor=' hsl(270, 3%, 87%)'
+        e.target.expDateYear.style.borderColor=' hsl(270, 3%, 87%)'
+        expMonthCorrect=true
+    }
+
+    let cvcCorrect=false
+    if(CVC===''){
+        cvcSpan.style.display='flex'
+        e.target.CVC.style.borderColor='red'
+    }else{
+        cvcSpan.style.display='none'
+        e.target.CVC.style.borderColor='hsl(270, 3%, 87%)'
+        cvcCorrect=true
+    }
+
 })
