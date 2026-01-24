5a0c53ad24bd16c806be93ba991432bd7b36c503
Diana <140351052+Diana044@users.noreply.github.com>
2026-01-25 00:24:13 +0400

    commit

---
+++ level 133/homework/script.js
@@ -0,0 +1,19 @@
+let userAnsw=prompt('enter  name')
+let name='diana'
+if(userAnsw === name){
+    let ifCorrect='name is correct'
+    console.log(ifCorrect)
+}else{
+    let ifCorrect='name is uncorrect'
+    console.log(ifCorrect)
+
+}
+
+
+
+for(let i=0; i<=10; i++){
+    let count=0
+    count=i
+    let sub=count+i
+    console.log(sub)
+}
