ab2ee864aa8923bc2f1178d5e444d9f2cb049915
Diana <140351052+Diana044@users.noreply.github.com>
2026-03-05 16:36:30 +0400

    commit

--- level 127/homework/hw.js
+++ level 127/homework/hw.js
@@ -7,12 +7,14 @@ let millisecondsInDay = 1000 * 60 * 60 * 24
 let millisecondsInHour = 1000 * 60 * 60
 let millisecondsInMinute = 1000 * 60
 
-let incomingNewYear =new Date('2026-1-1')
 
-function newYearCounter(incomingYear){
+let now = new Date()
+let incomingNewYear = new Date(now.getFullYear() + 1, 0, 1)
+
+function newYearCounter(){
     let counterObject = new Date()
   
-    let millisecondsLeft = incomingNewYear - counterObject -14400000
+    let millisecondsLeft = incomingNewYear - counterObject
 
     if( Math.floor(millisecondsLeft / millisecondsInDay)<10){
         days.textContent=`0${ Math.floor(millisecondsLeft / millisecondsInDay)}`
@@ -20,46 +22,31 @@ function newYearCounter(incomingYear){
         days.textContent=Math.floor(millisecondsLeft / millisecondsInDay)
     }
 
-      if( Math.floor(millisecondsLeft / millisecondsInHour) % 24<10){
-         hours.textContent=`0${ Math.floor(millisecondsLeft / millisecondsInHour)}`
+    if( Math.floor(millisecondsLeft / millisecondsInHour) % 24<10){
+         hours.textContent=`0${ Math.floor(millisecondsLeft / millisecondsInHour) % 24}`
     }else{
          hours.textContent=Math.floor(millisecondsLeft / millisecondsInHour) % 24
     }
 
-
-      if(Math.floor(millisecondsLeft / millisecondsInMinute) % 60<10){
+    if(Math.floor(millisecondsLeft / millisecondsInMinute) % 60<10){
        mins.textContent=`0${Math.floor(millisecondsLeft / millisecondsInMinute) % 60}`
     }else{
         mins.textContent=Math.floor(millisecondsLeft / millisecondsInMinute) % 60
     }
 
-
-      if( Math.floor(millisecondsLeft / 1000) % 60<10){
+    if( Math.floor(millisecondsLeft / 1000) % 60<10){
          secs.textContent=`0${Math.floor(millisecondsLeft / 1000) % 60}`
     }else{
         secs.textContent= Math.floor(millisecondsLeft / 1000) % 60
     }
-
-
-
 }
-
-// newYearCounter(new Date(counterObject.getFullYear() + 1, 0, 1))
 
 setInterval(newYearCounter, 1000)
 
-
-
-let input=document.getElementById('input')
-
-input.addEventListener('change',function(e){
-
-    incomingNewYear=new Date(e.target.value)
-
-
-    
+let input = document.getElementById('input')
+input.addEventListener('change', function(e){
+    incomingNewYear = new Date(e.target.value)
 })
-
 
 let clockSound = document.getElementById("clockSound")
 setInterval(function(){
@@ -71,5 +58,4 @@ jingle.play()
 
 setInterval(function(){
     jingle.play()
-}, 1000 * 117)
-
+}, 117000)
