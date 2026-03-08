c9cb50af3a480e9bae1f759000d97978a9051780
Diana044 <140351052+Diana044@users.noreply.github.com>
2026-03-09 01:28:40 +0400

    commit

---
+++ level 145/homework/hw1.js
@@ -0,0 +1,148 @@
+// 1)მოცემულია ობიექტი:
+
+// const company = {
+//   name: "TechCorp",
+//   location: {
+//     country: "Georgia",
+//     city: {
+//       name: "Tbilisi",
+//       street: "Rustaveli"
+//     }
+//   },
+//   employees: [
+//     { name: "Nika", age: 22 },
+//     { name: "Giorgi", age: 25 }
+//   ]
+// }
+
+// დესტრუქციის გამოყენებით ერთ ლაინში გამოიტანე კონსოლში:
+
+// company name
+
+// country
+
+// city name
+
+// street
+
+// პირველი employee-ს name
+
+// მეორე employee-ს age
+
+
+// 2)const university = {
+//   name: "TSU",
+//   faculty: {
+//     title: "Computer Science",
+//     head: {
+//       name: "Irakli",
+//       age: 50
+//     }
+//   },
+//   students: [
+//     {
+//       name: "Ana",
+//       grades: {
+//         math: 90,
+//         js: 95
+//       }
+//     }
+//   ]
+// }
+
+// დესტრუქციის გამოყენებით გამოიტანე კონსოლში:
+
+// university name
+
+// faculty title
+
+// head name
+
+// student name
+
+// math grade
+
+// js grade
+
+
+
+// 3)const store = {
+//   name: "MegaShop",
+//   address: {
+//     city: "Batumi",
+//     location: {
+//       street: "Chavchavadze",
+//       number: 10
+//     }
+//   },
+//   products: [
+//     {
+//       name: "Laptop",
+//       price: 2500
+//     },
+//     {
+//       name: "Phone",
+//       price: 1200
+//     }
+//   ]
+// }
+// დესტრუქციით გამოიტანე:
+
+// store name
+
+// city
+
+// street
+
+// first product name
+
+// second product price
+
+
+// 4)const user = {
+//   name: "Nika",
+//   surname: "Beridze",
+//   address: {
+//     country: "Georgia",
+//     city: {
+//       name: "Tbilisi",
+//       street: "Pekini"
+//     }
+//   }
+// }
+
+// 📌 დესტრუქციის გამოყენებით გამოიტანე:
+
+// name
+
+// surname
+
+// country
+
+// city name
+
+// street
+
+// 5)const car = {
+//   brand: "BMW",
+//   model: "M5",
+//   engine: {
+//     type: "V8",
+//     power: {
+//       hp: 600,
+//       torque: 750
+//     }
+//   }
+// }
+
+// დესტრუქციის გამოყენებით გამოიტანე:
+
+// brand
+
+// model
+
+// engine type
+
+// hp
+
+// torque
