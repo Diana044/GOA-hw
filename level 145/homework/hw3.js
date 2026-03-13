51f06eee99c8b1bbac826124208cec0caf146db8
Diana Dzukaevi <dzukaevadiana3@gmail.com>
2026-03-13 17:26:44 +0400

    commit

--- level 145/homework/hw3.js
+++ level 145/homework/hw3.js
@@ -1,32 +1,31 @@
-// 3)const store = {
-//   name: "MegaShop",
-//   address: {
-//     city: "Batumi",
-//     location: {
-//       street: "Chavchavadze",
-//       number: 10
-//     }
-//   },
-//   products: [
-//     {
-//       name: "Laptop",
-//       price: 2500
-//     },
-//     {
-//       name: "Phone",
-//       price: 1200
-//     }
-//   ]
-// }
 // დესტრუქციით გამოიტანე:
-
 // store name
-
 // city
-
 // street
-
 // first product name
-
 // second product price
 
+const store = {
+  name: "MegaShop",  
+  address: {
+    city: "Batumi",  
+    location: {
+      street: "Chavchavadze",  
+      number: 10
+    }  
+  },  
+  products: [
+    {
+      name: "Laptop",  
+      price: 2500
+    },  
+    {
+      name: "Phone",  
+      price: 1200 
+    }  
+]    
+}
+
+const {name,address:{city,location:{street}},products:[{name:product1Name},{price}]} =store
+
+console.log(name, city, street, product1Name, price)
--- level 145/homework/hw5.js
+++ level 145/homework/hw5.js
@@ -1,23 +1,35 @@
-// 5)const car = {
-//   brand: "BMW",
-//   model: "M5",
-//   engine: {
-//     type: "V8",
-//     power: {
-//       hp: 600,
-//       torque: 750
-//     }
-//   }
-// }
-
 // დესტრუქციის გამოყენებით გამოიტანე:
-
 // brand
-
 // model
-
 // engine type
-
 // hp
-
 // torque
+
+
+const car = {
+  brand: "BMW",
+  model: "M5",
+  engine: {
+    type: "V8",
+    power: {
+      hp: 600,
+      torque: 750
+    }
+  }
+}
+
+const {
+    brand,
+    model,
+    engine:{
+        type,
+        power:{
+            hp,
+            torque
+        }
+    }
+} = car
+
+console.log(car.brand,car.model, car.engine.type,car.engine.power.hp,car.engine.power.torque)
+
+console.log(brand, model, type, hp, torque)
