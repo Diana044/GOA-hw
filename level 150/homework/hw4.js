3fc304a66e7bd1e9b041b4987ee3dc2bd0cae673
Diana Dzukaevi <dzukaevadiana3@gmail.com>
2026-03-26 00:26:17 +0400

    commit

--- level 150/homework/hw4.js
+++ level 150/homework/hw4.js
@@ -1,4 +1,20 @@
 // 4)შექმენი კლასი Product, რომელსაც ექნება:
 
 // name, price, quantity
-// მეთოდი getTotalPrice() → აბრუნებს მთლიან ფასს(ფასი გამრავლებული ოდენობაზე)
+// მეთოდი getTotalPrice() →
+//  აბრუნებს მთლიან ფასს(ფასი გამრავლებული ოდენობაზე)
+
+class Product{
+    constructor(name, price, quantity){
+        this.productName=name
+        this.productPrice=price
+        this.productQuantity=quantity
+    }
+
+    getTotalPrice(){
+        return this.productPrice*this.productQuantity
+    }
+}
+
+const product1 = new Product('car',40000, 2)
+console.log(product1.getTotalPrice())
