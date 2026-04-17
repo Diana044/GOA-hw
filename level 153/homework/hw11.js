37bbb3ebd6cfcf315e41b3b123064afb32103d42
Diana Dzukaevi <dzukaevadiana3@gmail.com>
2026-04-17 21:19:47 +0400

    commit

--- level 153/homework/hw11.js
+++ level 153/homework/hw11.js
@@ -1,17 +1,12 @@
 
 // 11)გაქვს:
-
 let map = new Map([
   ["a", 10],
   ["b", 20],
   ["c", 30]
 ]);
-
 // იპოვე:
-
 // ყველა value-ს საშვალო
-
-
 let sui=0
 map.forEach((value)=>{
     sui+=value
--- level 153/homework/hw13.js
+++ level 153/homework/hw13.js
@@ -8,11 +8,9 @@ let numbers = [1,2,3,4,5,6,7,8,9];
 // value = შესაბამისი რიცხვების მასივი
 
 
-
 let map= new Map()
 for(let i of numbers){
     map.set(i,i%2==0?'even':'odd')
 }
-
 
 console.log(map)
